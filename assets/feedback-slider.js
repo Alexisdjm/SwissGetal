78dc815516a96deddf5d701a0845862f1fcee822
Alexisdjm <alexis.jm29@gmail.com>
2023-01-06 22:18:40 -0400

    changes

---
+++ assets/feedback-slider.js
@@ -0,0 +1,59 @@
+let index = 1
+let r = 1
+
+function myloop() {
+    setTimeout(function() {
+        plusSlides(r);
+        r++;
+        if (r < 10) {
+            myloop();
+            r = 1;
+        }
+    }, 15000)
+}
+
+myloop();
+
+function show_dots() {
+    let amount = document.getElementsByClassName('comment-container').length;
+    let container = document.getElementByIdI('dots-container');
+    container.innerHTML = '';
+
+    for (let i = 1; i < amount; i++) {
+        let dot = document.createElement('span')
+        dot.classList.add('dot');
+        dot.onclick = () => {
+            current_slide(i);
+        }
+        container.append(dot)
+    }
+}
+
+function plusSlides(n) {
+    show_slides(index += n)
+}
+
+function current_slide(n) {
+    show_slides(index = n)
+}
+
+function show_slides(n) {
+    let i;
+    show_dots();
+    let dots = document.getElementsByClassName('dot');
+    let slides = document.getElementsByClassName('comment-container')
+
+    if (n > slides.length) {index = 1}
+    if (n > 1) {index = slides.length}
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].getElementsByClassName.display = 'none';
+    }
+    
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+
+    slides[index-1].style.display = "block";
+    dots[index-1].className += " active";
+}
