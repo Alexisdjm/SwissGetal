486eabe6fe7d3c335f216e6e347c02e0dbde4502
Alexisdjm <alexis.jm29@gmail.com>
2023-01-16 01:28:41 -0400

    changes

--- assets/interfaces.js
+++ assets/interfaces.js
@@ -3,12 +3,15 @@ let navlinks = document.getElementById('navbar-full-width')
 let icons_container = document.getElementById('link-icons')
 let window_ = document.getElementById('main-content')
 let first_container = document.getElementById('new-header-container')
+let lines = docuent.getElementById('mobile-button-lines');
 
 let toggle_menu = document.getElementById('toggle-menu');
 let ninja_menu = document.getElementById('left-hidden-menu-box');
 let displayed = false;
 
 toggle_menu.onclick = () => {
+
+    console.log("display status:" + displayed)
 
     if (displayed === 'false') {
         ninja_menu.style.visibility = 'visible';
@@ -22,13 +25,15 @@ window.addEventListener('scroll', () => {
     
     if ( window.scrollY > 0) {
         header.classList.add('white-new-header');
-        first_container.classList.add('new-padding-header')
+        first_container.classList.add('new-padding-header');
         navlinks.classList.add('white-header-links');
         icons_container.classList.add('white-header-icons');
+        lines.classList.add('.black-lines-button');
     } else {
         header.classList.remove('white-new-header');
         first_container.classList.remove('new-padding-header')
         navlinks.classList.remove('white-header-links');
         icons_container.classList.remove('white-header-icons');
+        lines.classList.remove('.black-lines-button');
     }
 })
