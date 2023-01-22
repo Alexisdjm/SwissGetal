ae7560e4a0b51a7f1b5be21ad9d0597ebb027b62
Alexisdjm <alexis.jm29@gmail.com>
2023-01-22 18:08:40 -0400

    changes

--- assets/interfaces.js
+++ assets/interfaces.js
@@ -12,6 +12,17 @@ let search_glass = document.getElementById('search-link-glass')
 let close_search = document.getElementById('close-search-form')
 let right_box = document.getElementById('right-search-form')
 
+const filter_menu = document.getElementById('product-filter-menu')
+let shop_by_link = document.getElementById('div-0')
+
+filter_menu.addEventListener('hover', () => {
+    if (!shop_by_link.classList.contains("is-open")) {
+        shop_by_link.classList.add('is-open')
+    } else {
+        shop_by_link.classList.remove('is-open')
+    }
+})
+
 toggle_menu.onclick = () => {
     ninja_menu.classList.add('is-open')
 }
