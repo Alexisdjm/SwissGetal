9979180c97e1a1fc60cf7bf0a9f04e766d46bb2d
Alexisdjm <alexis.jm29@gmail.com>
2023-01-23 00:37:25 -0400

    changes

--- assets/interfaces.js
+++ assets/interfaces.js
@@ -38,7 +38,7 @@ arr.forEach(function(element, index) {
         counter--
         if (counter < 1) {
             filter_menu.classList.remove('open-hover')
-            if (window.scrollY === 0) {
+            if (window.scrollY === 0 && !filter_menu.classList.contains('open-hover')) {
                 header.classList.remove('white-new-header');
                 navlinks.classList.remove('white-header-links');
                 icons_container.classList.remove('white-header-icons');
@@ -47,16 +47,6 @@ arr.forEach(function(element, index) {
         }
     }
 })
-
-// shop_by_link.addEventListener('mouseenter', () => {
-//     filter_menu.classList.add('open-hover')
-//     if (window.scrollY === 0) {
-//         header.classList.add('white-new-header');
-//         navlinks.classList.add('white-header-links');
-//         icons_container.classList.add('white-header-icons');
-//         lines.classList.add('black-lines-button');
-//     }
-// })
 
 toggle_menu.onclick = () => {
     ninja_menu.classList.add('is-open')
