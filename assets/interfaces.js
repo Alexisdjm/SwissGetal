85ab4e7aaf999e1cd0812b1812d1515fdd2a9b25
Alexisdjm <alexis.jm29@gmail.com>
2023-01-23 00:32:56 -0400

    changes

--- assets/interfaces.js
+++ assets/interfaces.js
@@ -24,23 +24,39 @@ const arr = [shop_by_link, filter_menu]
 arr.forEach(function(element, index) {
     element.onmouseenter = () => {
         counter++
+        if (counter > 0) {
+            filter_menu.classList.add('open-hover')
+            if (window.scrollY === 0) {
+                header.classList.add('white-new-header');
+                navlinks.classList.add('white-header-links');
+                icons_container.classList.add('white-header-icons');
+                lines.classList.add('black-lines-button');
+            }    
+        }
     }
     element.onmouseleave = () => {
         counter--
-        console.log(counter)
+        if (counter < 1) {
+            filter_menu.classList.remove('open-hover')
+            if (window.scrollY === 0) {
+                header.classList.remove('white-new-header');
+                navlinks.classList.remove('white-header-links');
+                icons_container.classList.remove('white-header-icons');
+                lines.classList.remove('black-lines-button');
+            }  
+        }
     }
-    console.log(counter)
 })
 
-shop_by_link.addEventListener('mouseenter', () => {
-    filter_menu.classList.add('open-hover')
-    if (window.scrollY === 0) {
-        header.classList.add('white-new-header');
-        navlinks.classList.add('white-header-links');
-        icons_container.classList.add('white-header-icons');
-        lines.classList.add('black-lines-button');
-    }
-})
+// shop_by_link.addEventListener('mouseenter', () => {
+//     filter_menu.classList.add('open-hover')
+//     if (window.scrollY === 0) {
+//         header.classList.add('white-new-header');
+//         navlinks.classList.add('white-header-links');
+//         icons_container.classList.add('white-header-icons');
+//         lines.classList.add('black-lines-button');
+//     }
+// })
 
 toggle_menu.onclick = () => {
     ninja_menu.classList.add('is-open')
