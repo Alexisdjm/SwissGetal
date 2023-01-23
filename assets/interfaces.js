196f0a8e92d40a4d41e36e87d8b2f23e75dfc1e9
Alexisdjm <alexis.jm29@gmail.com>
2023-01-23 01:00:57 -0400

    changes

--- assets/interfaces.js
+++ assets/interfaces.js
@@ -21,34 +21,7 @@ let counter = 0
 
 const arr = [shop_by_link, filter_menu]
 
-arr.forEach(function(element) {
-    element.onmouseenter = () => {
-        counter++
-        if (counter > 0) {
-            filter_menu.classList.add('open-hover')
-            if (window.scrollY === 0) {
-                header.classList.add('white-new-header');
-                navlinks.classList.add('white-header-links');
-                icons_container.classList.add('white-header-icons');
-                lines.classList.add('black-lines-button');
-            }    
-        }
-    }
-    element.onmouseleave = () => {
-        counter--
-        if (counter < 1) {
-            filter_menu.classList.remove('open-hover')
-            window.addEventListener('scroll', () => {
-                if (window.scrollY === 0 && counter < 1) {
-                    header.classList.remove('white-new-header');
-                    navlinks.classList.remove('white-header-links');
-                    icons_container.classList.remove('white-header-icons');
-                    lines.classList.remove('black-lines-button');
-                }  
-            })
-        }
-    }
-})
+
 
 toggle_menu.onclick = () => {
     ninja_menu.classList.add('is-open')
@@ -67,6 +40,35 @@ close_search.onclick = () => {
 }
 
 window.addEventListener('scroll', () => {
+
+    arr.forEach(function(element) {
+        element.onmouseenter = () => {
+            counter++
+            if (counter > 0) {
+                filter_menu.classList.add('open-hover')
+                if (window.scrollY === 0) {
+                    header.classList.add('white-new-header');
+                    navlinks.classList.add('white-header-links');
+                    icons_container.classList.add('white-header-icons');
+                    lines.classList.add('black-lines-button');
+                }    
+            }
+        }
+        element.onmouseleave = () => {
+            counter--
+            if (counter < 1) {
+                filter_menu.classList.remove('open-hover')
+                window.addEventListener('scroll', () => {
+                    if (window.scrollY === 0 && counter < 1) {
+                        header.classList.remove('white-new-header');
+                        navlinks.classList.remove('white-header-links');
+                        icons_container.classList.remove('white-header-icons');
+                        lines.classList.remove('black-lines-button');
+                    }  
+                })
+            }
+        }
+    })
     
     if ( window.scrollY > 0) {
         header_container.classList.add('top-0')
