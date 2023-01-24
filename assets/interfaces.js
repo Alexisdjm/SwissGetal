86fc64086e8aa7af460c0286bc59983f2e5f7cfb
Alexisdjm <alexis.jm29@gmail.com>
2023-01-24 17:03:52 -0400

    changes

--- assets/interfaces.js
+++ assets/interfaces.js
@@ -83,7 +83,7 @@ function showhide(element, css, box, vari) {
     }
 }
 
-fdocument.getElementById('product-filter-menu').onmouseenter = () => {console.log('hovered')}
+document.getElementById('product-filter-menu').onclick = () => {alert('hovered')}
 
 // showhide(shop_by_link, "about-us-hover", filter_menu, counter)
 showhide(filter_menu, "about-us-hover", filter_menu, counter)
