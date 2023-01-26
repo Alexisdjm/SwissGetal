7d13cdf37f65937cbf980e16b0f7f0f20db3f758
Alexisdjm <alexis.jm29@gmail.com>
2023-01-26 01:03:06 -0400

    changes

--- assets/interfaces.js
+++ assets/interfaces.js
@@ -114,18 +114,11 @@ window.addEventListener('scroll', () => {
     
     if ( window.scrollY > 0) {
         header_container.classList.add('top-0')
-        header.classList.add('white-new-header');
         first_container.classList.add('new-padding-header');
-        navlinks.classList.add('white-header-links');
-        icons_container.classList.add('white-header-icons');
-        lines.classList.add('black-lines-button');
+        handleCss('add')
     } else {
         header_container.classList.remove('top-0')
-        header.classList.remove('white-new-header');
         first_container.classList.remove('new-padding-header')
-        navlinks.classList.remove('white-header-links');
-        icons_container.classList.remove('white-header-icons');
-        lines.classList.remove('black-lines-button');
-        
+        handleCss('remove')
     }
 })
