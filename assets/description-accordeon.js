aeff4f7dc294476b7a49640037f2ee3ff2c16f95
Alexis Jimenez <Alexis.jm29@gmail.com>
2023-09-17 17:05:15 -0400

    accordeon

--- assets/description-accordeon.js
+++ assets/description-accordeon.js
@@ -1,1 +1,6 @@
-let accordeon = document.getElementById("container-acc");
+const accordeon = document.getElementById("container-acc");
+const content = document.getElementById("content-cc");
+
+accordeon.addEventListener("click", () => {
+    content.classList.toggle("grow")
+})
